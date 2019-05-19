0473c438e8b8dab0365d12d15b4654ad7c271c3c
isc30 <ivansanzcarasa@gmail.com>
2019-05-19 18:53:22 +0200

    add Clock demo

---
+++ demos/Demo.Serverside/HelloCanvas/Clock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Isc.Blazor.Canvas.Demo.Serverside
+{
+    public sealed class Clock : Surface2DComponent
+    {
+        [JSInvokable(nameof(Clock) + nameof(RequestFrameAsync))]
+        public override async Task<bool> RequestFrameAsync()
+        {
+            Context.Clear("white");
+
+            Context.SetFillStyle("black");
+            Context.SetFont("24px Arial");
+            Context.FillText(DateTime.Now.Millisecond.ToString(), 25, 25);
+
+            await Context.RenderAsync();
+
+            return true;
+        }
+    }
+}
--- src/Blazor.Canvas/Canvas2DContext/Canvas2DContextExtensions.Draw.cs
+++ src/Blazor.Canvas/Canvas2DContext/Canvas2DContextExtensions.Draw.cs
@@ -11,5 +11,17 @@
         {
             context.PushAction(CanvasContextAction.Call("fill"));
         }
+
+        public static void Clear(this Canvas2DContext context,
+            string style = "transparent")
+        {
+            context.Save();
+            context.SetGlobalCompositeOperation("copy");
+            context.SetStrokeStyle(style);
+            context.BeginPath();
+            context.LineTo(0, 0);
+            context.Stroke();
+            context.Restore();
+        }
     }
 }
